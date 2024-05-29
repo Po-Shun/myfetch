cc4656f2bd06c6205947603d226c6852963b28e2
Po-Shun <a609071103@gmail.com>
2024-05-29 17:05:30 +0800

    [Finish the build sceheme]

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create the build directory if it doesn't exist
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+# Navigate to the build directory
+cd build
+
+# Run CMake from the build directory
+cmake ..
+
+# Build the project using the default build tool
+cmake --build .
+
